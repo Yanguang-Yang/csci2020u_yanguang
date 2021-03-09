d37eb5439731dd74bd409e4db3d6dbda180aa8fe
Yanguang-Yang <yyg522743064@gmail.com>
2021-03-10 05:35:41 +0800

    1

--- lab06/src/sample/Controller.java
+++ lab06/src/sample/Controller.java
@@ -1,4 +1,76 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+
 public class Controller {
+    private static double[] avgHousingPricesByYear = { 247381.0,264171.4,287715.3,294736.1, 308431.4,322635.9,340253.0,363153.7};
+    private static double[] avgCommercialPricesByYear = { 1121585.3,1219479.5,1246354.2,1295364.8, 1335932.6,1472362.0,1583521.9,1613246.3};
+    //private static String[] ageGroups = { "18-25", "26-35", "36-45", "46-55", "56-65", "65+"};
+    private static int[] purchasesByAgeGroup = { 648, 1021, 2453, 3173, 1868, 2247};
+    private static Color[] pieColours = { Color.AQUA, Color.GOLD, Color.DARKORANGE, Color.DARKSALMON, Color.LAWNGREEN, Color.PLUM};
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private GraphicsContext gc;
+
+    @FXML
+    private void initialize() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        //double[] data = {1,2,3,4,5,6,7,8};
+        //double[] data2 = {0,1,2,3,4,5,6,7};
+
+        //drawBarChart(100,300,avgHousingPricesByYear,Color.RED,0);
+        //drawBarChart(100,300,avgCommercialPricesByYear,Color.BLUE,100/avgCommercialPricesByYear.length);
+
+        drawPieChart(gc);
+    }
+/*
+    private void drawBarChart(int w, int h, double[] data, Color colour,int startX) {
+        gc.setFill(colour);
+        double xInc = (data.length);
+
+        double maxVal = Double.NEGATIVE_INFINITY;
+        double minVal = Double.MAX_VALUE;
+
+        for(double val: data){
+            if(val > maxVal){
+                maxVal = val;
+            } if(val < minVal){
+                minVal = val;
+            }
+        }
+
+        double x = startX;
+        for(double val: data){
+            double height = ((val-minVal) / (maxVal - minVal)) * h;
+            gc.fillRect(x,(h-height),xInc,height);
+            x += 2 * xInc;
+        }
+    }
+
+ */
+
+    private void drawPieChart(GraphicsContext gc) {
+        int numOfStudents = 0;
+        for (int studentsForDegree: purchasesByAgeGroup)
+            numOfStudents += studentsForDegree;
+
+        double startAngle = 0.0;
+        for (int i = 0; i < purchasesByAgeGroup.length; i++) {
+            double slicePercentage = (double) purchasesByAgeGroup[i] / (double) numOfStudents;
+            double sweepAngle = slicePercentage * 360.0;
+
+            gc.setFill(pieColours[i]);
+            gc.fillArc(350, 150, 300, 300, startAngle, sweepAngle, ArcType.ROUND);
+
+            startAngle += sweepAngle;
+        }
+    }
 }
--- lab06/src/sample/Main.java
+++ lab06/src/sample/Main.java
@@ -1,91 +1,22 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.chart.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
-import javafx.stage.Stage;
-import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    private static double[] avgHousingPricesByYear = { 247381.0,264171.4,287715.3,294736.1,308431.4,322635.9,340253.0,363153.7};
-    private static double[] avgCommercialPricesByYear = { 1121585.3,1219479.5,1246354.2,1295364.8, 1121585.3,1219479.5,1246354.2,1295364.8,};
-    private static String[] ageGroups = { "18-25", "26-35", "36-45", "46-55", "56-65", "65+"};
-    private static int[] purchasesByAgeGroup = { 648, 1021, 2453, 3173, 1868, 2247};
-    private static Color[] pieColours = { Color.AQUA, Color.GOLD, Color.DARKORANGE, Color.DARKSALMON, Color.LAWNGREEN, Color.PLUM};
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Lab 06");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
-
-
-
-    @Override public void start(Stage stage) {
-        stage.setTitle("Bar Chart");
-        final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        final BarChart<String,Number> bc =
-                new BarChart<String,Number>(xAxis,yAxis);
-
-        XYChart.Series series1 = new XYChart.Series();
-        series1.setName("avgHousingPricesByYear");
-        series1.getData().add(new XYChart.Data("1", avgHousingPricesByYear[0]));
-        series1.getData().add(new XYChart.Data("2", avgHousingPricesByYear[1]));
-        series1.getData().add(new XYChart.Data("3", avgHousingPricesByYear[2]));
-        series1.getData().add(new XYChart.Data("4", avgHousingPricesByYear[3]));
-        series1.getData().add(new XYChart.Data("5", avgHousingPricesByYear[4]));
-        series1.getData().add(new XYChart.Data("6", avgHousingPricesByYear[5]));
-        series1.getData().add(new XYChart.Data("7", avgHousingPricesByYear[6]));
-        series1.getData().add(new XYChart.Data("8", avgHousingPricesByYear[7]));
-
-        XYChart.Series series2 = new XYChart.Series();
-        series2.setName("avgCommercialPricesByYear");
-        series2.getData().add(new XYChart.Data("1", avgCommercialPricesByYear[0]));
-        series2.getData().add(new XYChart.Data("2", avgCommercialPricesByYear[1]));
-        series2.getData().add(new XYChart.Data("3", avgCommercialPricesByYear[2]));
-        series2.getData().add(new XYChart.Data("4", avgCommercialPricesByYear[3]));
-        series2.getData().add(new XYChart.Data("5", avgCommercialPricesByYear[4]));
-        series2.getData().add(new XYChart.Data("6", avgCommercialPricesByYear[5]));
-        series2.getData().add(new XYChart.Data("7", avgCommercialPricesByYear[6]));
-        series2.getData().add(new XYChart.Data("8", avgCommercialPricesByYear[7]));
-
-        Scene scene  = new Scene(bc,800,600);
-        bc.getData().addAll(series1, series2);
-        stage.setScene(scene);
-        stage.show();
-
-        //Scene scene = new Scene(new Group());
-        //stage.setTitle("Imported Fruits");
-        //stage.setWidth(500);
-        // stage.setHeight(500);
-
-        //Scene scene = new Scene(new Group());
-        //stage.setTitle("Imported Fruits");
-        //stage.setWidth(500);
-        //stage.setHeight(500);
-
-        //ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                        new PieChart.Data(ageGroups[0], purchasesByAgeGroup[0]),
-                        new PieChart.Data(ageGroups[1], purchasesByAgeGroup[1]),
-                        new PieChart.Data(ageGroups[2], purchasesByAgeGroup[2]),
-                        new PieChart.Data(ageGroups[3], purchasesByAgeGroup[3]),
-                        new PieChart.Data(ageGroups[4], purchasesByAgeGroup[4]));
-        //final PieChart chart = new PieChart(pieChartData);
-        //chart.setTitle("Imported Fruits");
-
-        //((Group) scene.getRoot()).getChildren().add(chart);
-        //stage.setScene(scene);
-        //stage.show();
-    }
-
-
 }
